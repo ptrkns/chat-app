7069ee3a7932bbdf2abbbb4b5342400909195be9
ptrkns <keneseipeterr@gmail.com>
2024-06-12 19:08:46 +0200

    useNavigate in the Login form

--- chat-app-front-end/src/components/Login.jsx
+++ chat-app-front-end/src/components/Login.jsx
@@ -1,28 +1,35 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import '../styles/Login.css';
 import UserContext from './UserContext';
 
 function Login() {
   const {userName, setUserName} = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/home');
+  }
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
         <div className='Login-Parent'>
             <div className='Login-Title-Container'>
                 <h1>React Chat App</h1>
             </div>
             <div className='Login-Input-Container'>
                 <input
+                  required
                   type='text'
                   placeholder='Enter a unique username'
                   onChange={(e) => setUserName(e.target.value)}
                 ></input>
-                <Link to='/home'>Login</Link>
+                <button type='submit'>Login</button>
             </div>
         </div>
-    </>
+    </form>
   );
 }
 
