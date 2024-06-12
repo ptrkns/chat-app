834d17855128279d88230578869c3c25b0dac65e
ptrkns <keneseipeterr@gmail.com>
2024-06-12 20:50:56 +0200

    Connected the frontend to the server

--- chat-app-front-end/src/components/ChatComponent.jsx
+++ chat-app-front-end/src/components/ChatComponent.jsx
@@ -1,27 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import '../styles/ChatComponent.css';
-import Messages from './Messages.jsx';
+import Message from './Message.jsx';
 import UserContext from './UserContext';
+import { Link } from 'react-router-dom';
+import socketIOClient from 'socket.io-client';
 
 function ChatComponent() {
 
     const { userName } = useContext(UserContext);
+    const socketio = socketIOClient('http://localhost:3001');
+    const [chat, setChat] = useState([]);
+    const [message, setMessage] = useState('');
+    useEffect(() => {
+        socketio.on('chat', (messages) => {
+            setChat(messages);
+        });
+    });
+
+    const sendToSocket = (chat) => {
+        socketio.emit('chat', chat);
+    }
+
+    const sendMessage = () => {
+        addMessage({message});
+        setMessage('');
+    }
+
+    const addMessage = (message) => {
+        const newChat = {...message, user: userName}
+        setChat([...chat, newChat]);
+        sendToSocket([...chat, newChat]);
+    }
 
     return (
         <>
             <div className='ChatContainer-Parent'>
                 <div className='ChatContainer-Header'>
                     <p>{userName}</p>
-                    <button>Log out</button>
+                    <Link to='/' className='Link-Button'>Log out</Link>
                 </div>
 
                 <div className='ChatContainer-Body'>
-                    <Messages/>
+                    {
+                        chat.map((chat, index) => {
+                            if(chat.user === userName) {
+                                return <Message type='sent' key={index} userName={chat.user} message={chat.message}/>
+                            }
+                            else if(chat.user !== userName) {
+                                return <Message type='received' key={index} userName={chat.user} message={chat.message}/>
+                            }
+                        })
+                    }
                 </div>
 
                 <div className='ChatContainer-Input'>
-                    <textarea></textarea>
-                    <button>Send</button>
+                    <textarea onChange={(e) => setMessage(e.target.value)}/>
+                    <button onClick={sendMessage}>Send</button>
                 </div>
             </div>
         </>
